88e74181ab6bd9124bef98f869ccd0a35102ee6e
Your Name <chantiambati411@gmail.com>
2025-01-25 13:37:50 +0530

    my changes done check mr sai

--- login.js
+++ login.js
@@ -1,1 +1,37 @@
-console.log('Hello World');
+
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const usernameField = document.getElementById('username');
+    const passwordField = document.getElementById('password');
+    const messageElement = document.getElementById('message');
+
+    const username = usernameField.value;
+    const password = passwordField.value;
+
+   
+
+    if (username === "" || password === "") {
+        messageElement.textContent = "Please enter  username and password.";
+        messageElement.style.color = "red";
+        
+    } else if (username === "user" && password === "pass") { 
+        messageElement.textContent = "Login successful!";
+        messageElement.style.color = "green";
+        window.location.href = 'blank.html';
+
+        }
+    
+    else {
+        messageElement.textContent = "Invalid username or password.";
+        messageElement.style.color = "red"; 
+
+        usernameField.value = "";
+        passwordField.value = "";
+
+    }
+    setTimeout(() => {
+        messageElement.textContent = "";
+    }, 2000);
+});
